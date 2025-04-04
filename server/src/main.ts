46f121f0f139b2bbc0860db906b8e76d11e35a8c
Wassini Bouzidi <wassini.bouzidi@epitech.eu>
2025-04-04 23:42:03 +0200

    feat: add axios for HTTP requests and implement health check ping in main application loop

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
+import axios, { AxiosError } from 'axios';
 
 dotenv.config();
 
@@ -9,20 +10,41 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  // Configuration Swagger avant de démarrer l'application
   const config = new DocumentBuilder()
     .setTitle('API Paradox')
     .setDescription("Documentation de l'API Paradox")
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
   const port = process.env.PORT || 5000;
   await app.listen(port);
-
   console.log(`Application is running on: ${await app.getUrl()}`);
+
+  setInterval(() => {
+    void (async () => {
+      try {
+        const appUrl = await app.getUrl();
+        if (typeof appUrl === 'string') {
+          try {
+            await axios
+              .get(`${appUrl}/`)
+              .then(() => console.log('Ping OK'))
+              .catch((error: AxiosError) => {
+                console.error('Ping failed', error.message);
+              });
+          } catch (error) {
+            console.error('Ping failed', error);
+          }
+        } else {
+          console.error('Invalid app URL');
+        }
+      } catch (error) {
+        console.error('Ping échoué', error);
+      }
+    })();
+  }, 870000);
 }
-bootstrap();
+void bootstrap();
